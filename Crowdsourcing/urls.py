a06cb0464bc364e9b794b2b64cb94c89d5db9e87
QYJ77 <1160206543@qq.com>
2019-09-19 11:44:33 +0800

    Add page1 by QYJ

--- Crowdsourcing/urls.py
+++ Crowdsourcing/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('page1/',views.page1)
+    path('page4/',views.page4)
 ]
--- creditmanagement/views.py
+++ creditmanagement/views.py
@@ -5,3 +5,5 @@ def page2(request):
     return render(request,'page2.html')
 def page1(request):
     return render(request,'page1.html')
+def page4(request):
+    return render(request,'page4.html')
