a439d3b9295c341bfc8402981cbf17f124638c15
Spiderman321 <834057098@qq.com>
2019-10-29 13:42:59 +0800

    Add models by zhangweijie

--- creditmanagement/models.py
+++ creditmanagement/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Sponsor(models.Model):
+    id = models.AutoField();　
+    name = models.CharField(max_length=128);
+    phoneNum = models.IntegerField;
+
+
+    def __str__(self):
+        return self.name;
